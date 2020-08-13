17476e370b9af5fbb0deba26fc54fd4db19df4d5
Pipicallu <thomas.vassallo@gmail.com>
2020-08-13 15:39:08 +0000

    added hi score button and acompanying onclick event to show case hi scores

--- JS/index.js
+++ JS/index.js
@@ -8,8 +8,22 @@ $(document).ready(function(){
      $("#cadetButton").animate({bottom: '0px'}, 550);
      $("#pilotButton").animate({bottom: '0px'}, 800);
      $("#commanderButton").animate({bottom: '0px'}, 1000);
+     $("#hiScoreButton").animate({bottom: '0px'}, 1200);
 });
 
 
-/*-- cardback animation --*/ 
+
+  function showHiScores(){
+      
+     var x = document.getElementById("hiScoreOverlay");
+      if(x.style.display === ""){
+         x.style.display = "block";
+      } else if (x.style.display === "none") {
+          x.style.display = "block";
+      } else{
+          x.style.display = "none";
+      }
+
+  }
+
 
